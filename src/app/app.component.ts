b7d8676ac1955eb9d7f920a43eed088548e3a3be
Eve Ishimwe <eveishimwe1@gmail.com>
2019-08-23 15:56:03 +0200

    added in app.component.html.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
 export class AppComponent {
-  goals:string[];
-
-  constructor(){
-    this.goals = ['Watch finding Nemo', 'Buy cookies', 'Get new phone case']
-  } 
+  quotes:Quote[] = [
+    {id:1, name:'Watch finding Nemo'},
+    {id:2,name:'Buy Cookies'},
+    {id:3,name:'Get new Phone Case'},
+    {id:4,name:'Get Dog Food'},
+    {id:5,name:'Solve math homework'},
+    {id:6,name:'Plot my world domination plan'},
+  ];
 }
